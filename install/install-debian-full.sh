238883d4ade6b01588d80a7e8597765e160648e5
TomBombadilom <tom@croll.fr>
2024-03-28 22:57:00 +0100

    added missing dependencies

--- install/install-debian-full.sh
+++ install/install-debian-full.sh
@@ -7,6 +7,26 @@ then
 fi
 
 git clone https://github.com/JaKooLit/Debian-Hyprland.git
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+source ~/.zshrc
+nvm install 21
+npm i -g typescript
+
 cd Debian-Hyprland
 
 source install.sh
+
+git clone  https://github.com/JaKooLit/Debian-Hyprland.git
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.7/install.sh | bash
+source ~/.bashrc
+source ~/.zshrc
+nvm install 21
+npm i -g typescript
+sudo apt install -y libpulse-dev libgjs-dev axel libtinyxml2 libgtk-mm-3.0-dev libgtksourceviewmm-3.0-dev
+cd Debian-Hyprland
+
+source install.sh
+git clone https://github.com/end-4/dots-hyprland.git
+cd dots-hyprland
+source non-Arch-installer.temp.sh

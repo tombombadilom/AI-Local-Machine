f2b4512670c2766252bf62d2b9d8412e0133c1dd
TomBombadilom <tom@croll.fr>
2024-03-28 20:10:17 +0100

    added AnythingLLM install

--- install/app-install.sh
+++ install/app-install.sh
@@ -12,3 +12,19 @@ install() {
     done
 }
 install "thunar blas-openblas brave-bin vim jan-bin ollama lmstudio-appimage localai-git"
+
+## download via curl this  appimage 
+## then install it making a ./local/share/application/AnythingLLM.desktop 
+curl -L https://s3.us-west-1.amazonaws.com/public.useanything.com/latest/AnythingLLMDesktop.AppImage -o AnythingLLMDesktop.AppImage
+mkdir -p ~/.local/share/applications
+cat > ~/.local/share/applications/AnythingLLM.desktop <<EOF
+[Desktop Entry]
+Type=Application
+Name=Anything LLM
+Exec=$HOME/.local/bin/AnythingLLMDesktop.AppImage
+Icon=$HOME/.local/bin/AnythingLLMDesktop.AppImage
+Terminal=false
+Categories=Utility;Application;
+EOF
+
+

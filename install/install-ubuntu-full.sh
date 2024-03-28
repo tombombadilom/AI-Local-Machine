1f8c8064e429d2b732aea76f450417854a3cbbf6
TomBombadilom <tom@croll.fr>
2024-03-28 20:47:08 +0100

    started debian and ubuntu hyprland install script

---
+++ install/install-ubuntu-full.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+## check if git is installed if not install it
+if ! command -v git &> /dev/null
+then
+    echo "git n'est pas installé"
+    sudo apt-get install -y git
+fi
+
+git https://github.com/JaKooLit/Debian-Hyprland.git
+cd Debian-Hyprland
+
--- start.sh
+++ start.sh
@@ -1,10 +1,43 @@
 #!/usr/bin/env bash
+## check the os of the machine (arch linux, debian , ubuntu )
+if [ -f /etc/arch-release ]; then
+  OS="arch"
+elif [ -f /etc/debian_version ]; then
+  OS="debian"
+  DEBIAN_VERSION=$(cat /etc/debian_version | cut -d '.' -f1 | cut -d '/' -f1)
+  if [ "$DEBIAN_VERSION" -lt "13" ] || [ "$DEBIAN_VERSION" -ge "14" ]; then
+    echo "Debian version not supported, please use Debian 10 (Buster) or higher"
+    exit 1
+  else
+    DEBIAN_RELEASE="trixie"
+  fi
+elif [ -f /etc/os-release ]; then
+  . /etc/os-release
+  if [ "$ID" == "ubuntu" ]; then
+    OS="ubuntu"
+    UBUNTU_VERSION=$(echo $VERSION_ID | cut -d '.' -f1)
+    if [ "$UBUNTU_VERSION" -lt "20" ]; then
+      echo "Ubuntu version not supported, please use Ubuntu 20.04 (Focal) or higher"
+      exit 1
+    else
+      UBUNTU_RELEASE="focal"
+    fi
+  fi
+fi
 
 read -p "Do you want to install only the AI applications or the whole Hyprland Display with the AI chat in it? (apps/full) " answer
 if [[ "${answer}" == "apps" ]]; then
   source ./install/install-apps.sh
 elif [[ "${answer}" == "full" ]]; then
-  source ./install/install-full.sh
+  if [ "$OS" == "debian" ] && [ "$DEBIAN_VERSION" -eq "10" ]; then
+    source ./install/install-debian-full.sh
+  elif [ "$OS" == "ubuntu" ] && [ "$UBUNTU_VERSION" -eq "20" ]; then
+    source ./install/install-ubuntu-full.sh
+  else
+    source ./install/install-full.sh
+  fi
+
 else
   echo "Please answer with 'apps' or 'full'"
 fi
+

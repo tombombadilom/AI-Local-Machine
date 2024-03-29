026918e888c4d073f6351f4aef7683ed7452a09a
TomBombadilom <tom@croll.fr>
2024-03-29 03:17:39 +0100

    added personnal config

--- install/install-ubuntu-full.sh
+++ install/install-ubuntu-full.sh
@@ -12,10 +12,44 @@ source ~/.bashrc
 source ~/.zshrc
 nvm install 21
 npm i -g typescript
-sudo apt install -y libpulse-dev libgjs-dev axel libtinyxml2 libgtk-mm-3.0-dev libgtksourceviewmm-3.0-dev
+sudo apt install -y libpulse-dev libgjs-dev axel libtinyxml2-dev libgtkmm-3.0-dev libgtksourceviewmm-3.0-dev
 cd Debian-Hyprland
 
 source install.sh
 git clone https://github.com/end-4/dots-hyprland.git
 cd dots-hyprland
 source non-Arch-installer.temp.sh
+bash
+declare -A config_git=(
+    ["$HOME/.config/ags"]="https://github.com/tombombadilom/ags"
+    ["$HOME/.config/hypr"]="https://github.com/tombombadilom/hypr"
+)
+
+# Boucler sur chaque répertoire et son URL
+for conf in "${!config_git[@]}"; do
+    url="${config_git[$conf]}"
+    echo "Vérification du répertoire: $conf"
+    # Vérifier si le répertoire existe
+    if [ -d "$conf" ]; then
+        echo "Le répertoire existe."
+        # Vérifier si le répertoire .git existe à l'intérieur
+        if [ -d "$conf/.git" ]; then
+            echo "Le répertoire .git existe. Exécution de git pull..."
+            # Se déplacer dans le répertoire et exécuter git pull
+            (cd "$conf" && git pull)
+        else
+            echo "Le répertoire .git n'existe pas. Clonage du dépôt $url..."
+            # Cloner le dépôt Git dans le répertoire spécifié
+            mv "$conf" "$conf.old"
+            git clone "$url" "$conf"
+        fi
+    else
+        echo "Le répertoire $conf n'existe pas. Création et clonage du dépôt $url..."
+        # Cloner le dépôt Git dans le répertoire spécifié
+        git clone "$url" "$conf"
+    fi
+done
+
+cp ../config/config.fish ~/.config/fish/.
+cp ../config/foot.ini ~/./config/foot/.
+source ./app-install.sh
